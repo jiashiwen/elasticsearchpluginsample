359c0508f775b7d618fdf6e0a8ddd49b2795a1ba
jiashiwen <jiashiwen126@126.com>
2018-01-02 21:07:54 +0800

    修改MyNativeScriptPlugin接收参数add的值，修改README

--- src/main/java/red/shiwen/firestEsPlugin/MyNativeScriptPlugin.java
+++ src/main/java/red/shiwen/firestEsPlugin/MyNativeScriptPlugin.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.common.Nullable;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.plugins.ScriptPlugin;
@@ -12,7 +14,13 @@ import org.elasticsearch.script.ExecutableScript;
 import org.elasticsearch.script.NativeScriptFactory;
 
 public class MyNativeScriptPlugin extends Plugin implements ScriptPlugin {
+    private final static Logger LOGGER = LogManager.getLogger(MyFirstPlugin.class);
 
+    public MyNativeScriptPlugin() {
+        super();
+        LOGGER.warn("This is MyNativeScriptPlugin");
+    }
+    
     @Override
     public List<NativeScriptFactory> getNativeScripts() {
         return Collections.singletonList(new MyNativeScriptFactory());
@@ -22,7 +30,20 @@ public class MyNativeScriptPlugin extends Plugin implements ScriptPlugin {
         @Override
         public ExecutableScript newScript(@Nullable Map<String, Object> params) {
 
-            return new MyNativeScript();
+//            return new MyNativeScript();
+            return new AbstractDoubleSearchScript(){
+
+                @Override
+                public double runAsDouble() {
+                    int b=0;
+                    if(params.get("add")!=null){
+                        b= (int) params.get("add");
+                    }
+
+                    String s =  source().get("last").toString();
+                    double a = s.length()+b;
+                    return a;                }
+            };
         }
 
         @Override
@@ -36,14 +57,14 @@ public class MyNativeScriptPlugin extends Plugin implements ScriptPlugin {
         }
     }
 
-    public static class MyNativeScript extends AbstractDoubleSearchScript {
-        @Override
-        public double runAsDouble() {
-            String s =  source().get("last").toString();
-            double a = s.length();
-//			double b = (double) source().get("b");
-            return a;
-        }
-    }
+//    public static class MyNativeScript extends AbstractDoubleSearchScript {
+//        @Override
+//        public double runAsDouble() {
+//
+//            String s =  source().get("last").toString();
+//            double a = s.length();
+//            return a;
+//        }
+//    }
 
 }
